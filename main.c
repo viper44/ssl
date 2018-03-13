3448c8c78d40168a514a1425ded7ad1ad402e4a7
viper44 <msemenov@e1r6p4.unit.ua>
2018-03-13 20:17:18 +0200

    add smth

--- main.c
+++ main.c
@@ -12,6 +12,8 @@
 
 #include "ssl.h"
 #include "libft/libft.h"
+#include <stdlib.h>
+#include <unistd.h>
 // void	ft_fill_commands(char **commands)
 // {
 // 	commands[0] = ft_strnew (6);
@@ -23,36 +25,134 @@
 // 	ft_strcmp(str, )
 // }
 
+// int		ft_reveres_itoa(char *str, base)
+// {
+// 	int ret;
+// 	int i;
+
+// 	i = 0;
+// 	ret = 0;
+// 	while (base >= 1)
+// 	{
+// 		if (str[i] == 1)
+// 			ret += base;
+// 		base /= 2;
+// 		i++;
+// 	}
+// 	return (ret);
+// }
+
+// void	ft_convert(t_data *data)
+// {
+
+// }
+
+// void	ft_encode(char *line, t_data *data)
+// {
+// 	char 	*str;
+// 	int		n;
+// 	int		i;
+
+// 	i = 0;
+// 	n = 0;
+// 	str = ft_strnew(6);
+// 	while (1)
+// 	{
+// 		while (line[n])
+// 		{
+// 			str[i++] = line[n++]
+// 			if (n % 5 == 0)
+// 			break; 
+// 		}
+// 		n++;		
+// 		if (ft_strlen(str) != 6)
+// 			while (ft_strlen(str) != 6)
+// 				str[i++] = '0'
+// 		i = 0;
+// 		data->ret = ft_reveres_itoa(str, 32);
+// 		ft_convert(data);
+// 	}
+// }
+
+char *ft_covert_2(int tmp, int base, char *buffer)
+{
+	int i;
+	char *str;
+	int n;
+
+	i = 128;
+	n = 8;
+	str = ft_strnew(n);
+	n -= 1;
+	while (n + 1)
+	{
+		str[n] = tmp % 2 + 48;
+		tmp /= 2;
+		n--;
+	}
+	//printf("str = %s\n", str);
+	free(buffer);
+	return (str);
+}
+
+char *ft_add_zero(char *line)
+{
+	int i;
+	int size;
+	char *ret;
+
+	i = 0;
+	size = 24 - (int)ft_strlen(line);
+	printf("%d\n", size);
+	ret = ft_strnew(size);
+	size -= 1;
+	while (size >= 0 )
+	{
+		ret[size] = '0';
+		size--;
+	}
+	line = ft_strjoin(line, ret);
+	free (ret);
+	return (line);
+}
+
 void	ft_read_input(t_data *data)
 {
 	char 	*buffer;
 	char 	*line;
 	int		ret;
+	int 	tmp;
 
 	ret = 1;
-	line = NULL;
+	line = ft_strnew(0);
 	buffer = ft_strnew(BUFF_SIZE);
 	while(ret > 0)
 	{
 		ret = read(0, buffer, BUFF_SIZE);
+		if (ret == 0)
+			break ;
 		if (ret == -1)
 			return ;
 		buffer[ret] = '\0';
+		tmp = *buffer;
+		buffer = ft_covert_2(tmp, 2, buffer);
 		line = ft_strjoin(line, buffer);
 	}
 	free (buffer);
+	printf("line == %s\n", line);
+	if ((ft_strlen(line) % 24))
+		line = ft_add_zero(line);
 	printf("%s\n", line);
-
+	//ft_encode(line, data);
 }
 
 int		main(int argc, char const **argv)
 {
-	//char **commands[5];
-	t_data *data;
+	t_data	*data;
+	int		i;
 
+	i = 0;
 	data = ft_memalloc(sizeof(t_data));
-	//commands[4] = NULL;
-	//ft_fill_commands(commands);
 	if (argc == 1)
 	{
 		printf("%s\n", "usage: ft_ssl command [command opts] [command args]");
--- ssl.h
+++ ssl.h
@@ -13,11 +13,21 @@
 #ifndef SSL_H
 # define SSL_H
 # include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft/libft.h"
 # define BUFF_SIZE 1
 
 typedef struct s_data
 {
-	int check;
+	int			ret;
+	int			*arr_ret;
+	int			*arr_base;
+	int			arr_size;
+	int			byte_counter;
 }			   t_data;
+
+
+char			*itoa_base(long long int value, int base);
 
 #endif
